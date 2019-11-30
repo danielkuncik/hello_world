715c216585a5bf8cff25830728c8bcb2581b4238
Daniel Kuncik <dkuncik@gmail.com>
2019-11-30 17:32:57 -0500

    add process.env.port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
 	res.send('Hello World! Even if Brady is getting older, he\'ll be the GOAT until someone else wins six.');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('App is up on port 3000');
 });
